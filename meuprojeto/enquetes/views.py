de01d5e2932c83fccb15449bacda1e357cc8c2e3
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2020-08-19 20:27:30 -0300

    adição de rota 'aluno' usando render no html

--- meuprojeto/enquetes/views.py
+++ meuprojeto/enquetes/views.py
@@ -1,8 +1,14 @@
 from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def welcome(request):
     template = loader.get_template('welcome.html')
     return HttpResponse(template.render({}, request))
 
+def student(request):
+    context = {
+        'student': 'luiz junior',
+    }
+    return render(request, 'student/index.html', context)
