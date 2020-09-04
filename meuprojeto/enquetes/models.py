ae569e10b514513030dffd717fecb97444a2455b
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2020-09-04 18:34:36 -0300

    Atualização das models p/ models padrões

--- meuprojeto/enquetes/models.py
+++ meuprojeto/enquetes/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 
-class Enquete(object):
-    def __init__ (self,  id = 0, texto = "", data_publicacao="01/01/2000",):
-        self.id = id
-        self.texto = texto
-        self.data_publicacao = data_publicacao
+class Enquete(models.Model):
+    texto = models.CharField(max_length=100)
+    data_publicacao = models.DateField()
--- meuprojeto/enquetes/views.py
+++ meuprojeto/enquetes/views.py
@@ -8,12 +8,6 @@ def welcome(request):
     return HttpResponse(template.render({}, request))
 
 def exibir_enquete(request, enquete_id):
-    enquete = Enquete()
-    if enquete_id == 1:
-        enquete = Enquete(enquete_id,"Por que o céu é azul?","26/10/1989")
-    if enquete_id == 2:
-        enquete = Enquete(enquete_id,"Tem sempre alguma coisa por dizer","23/07/1989")
-    if enquete_id == 3:
-        enquete = Enquete(enquete_id,"Como o velho jonh dizia, a vida é um cabaré?","02/12/2013")
+    enquete = Enquete.objects.get(id=enquete_id)
     context = {'enquete':enquete}
     return render(request,'enquete.html',context)
